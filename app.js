9fd1363c9f76cc63d9c17648cdedc2840987e5df
Ashley Ng <ashley.ng@utexas.edu>
2015-08-07 16:38:42 -0500

    add to make slack connect, hopefully

--- app.js
+++ app.js
@@ -3,6 +3,38 @@ var gilbot = require('./gilbot');
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var Slack = require('slack-client');
+
+var token = 'xoxb-8766272068-HcErbipxskzfTa8ea1rX5YvZ'
+
+var slack = new Slack(token, true, true);
+
+slack.on('open', function () {
+    var channels = Object.keys(slack.channels)
+        .map(function (k) { return slack.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+ 
+    var groups = Object.keys(slack.groups)
+        .map(function (k) { return slack.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+ 
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+ 
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+ 
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+});
+ 
+slack.login();
  
 var app = express();
 var port = process.env.PORT || 3000;

bf71b4fb0cb0baf4b91e7bed5ca41597b5a839bf
Ashley Ng <ashley.ng@utexas.edu>
2015-08-07 17:00:11 -0500

    hopefully get rid of recursion

--- gilbot.js
+++ gilbot.js
@@ -3,9 +3,9 @@ module.exports = function (req, res, next) {
   var botPayload = {
     text : 'Dude, chill!'
   };
- 
+  var message = req.body.text;
   // avoid infinite loop
-  if (userName !== 'gilbot') {
+  if (userName !== 'gilbot' || message !== "Dude, chill!") {
     return res.status(200).json(botPayload);
   } else {
     return res.status(200).end();

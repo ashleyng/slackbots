01619cee4603a3cbf00247a99015a0f17a09d0c3
Ashley Ng <ashley.ng@utexas.edu>
2015-08-10 09:52:47 -0500

    add some more message

--- app.js
+++ app.js
@@ -13,17 +13,28 @@ slack.on('message', function(message) {
   var user = slack.getUserByID(message.user);
   var messagetext = message.text
   var channel = slack.getChannelGroupOrDMByID(message.channel);
-  var lowercase = messagetext.toLowerCase();
-  var exclamationmarks = lowercase.match(/!/g);
 
-  if (channel.name !== 'allagile' && user.name !== 'gilbot') {
-    if (lowercase === "dude") {
-      channel.send('Dude, chill!') 
-    }
-    else if (exclamationmarks && exclamationmarks.length >= 2) {
-      channel.send('Dude, chill!')
-    }
+  if (messagetext) {
+    var lowercase = messagetext.toLowerCase();
+    var exclamationmarks = lowercase.match(/!/g);
 
+    if (channel.name !== 'allagile' && user.name !== 'gilbot') {
+      if (lowercase === "dude") {
+        channel.send('Dude, chill.') 
+      }
+      else if (exclamationmarks && exclamationmarks.length >= 2) {
+        channel.send('Dude, chill.')
+      }
+      else if (lowercase.indexOf("lemonade") > -1 || lowercase.indexOf("milkshake") > -1) {
+        channel.send('Dude, lemonade and milkshakes.')
+      }
+      else if (lowercase.indexOf("smash") > -1) {
+        channel.send("Dude, Smash!");
+      }
+      else if (lowercase.indexOf("choo") > -1) {
+        channel.send("CHOO CHOO! :steam_locomotive:")
+      }
+    }
   }
 })
 
